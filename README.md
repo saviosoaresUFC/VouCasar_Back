e68ee977ff797f353f0325f242b6bb64e7725c6a
SavioUFC <saviocarvalho@alu.ufc.br>
2024-08-24 19:34:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VouCasar
+Vai casar? Esse é um software para seus convidados de casamento escolher qual presente dar a você!
---
+++ src/main/java/com/carvalhotechsolutions/voucasar/VouCasarApplication.java
@@ -0,0 +1,13 @@
+package com.carvalhotechsolutions.voucasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VouCasarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VouCasarApplication.class, args);
+    }
+
+}
